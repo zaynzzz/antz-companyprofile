1152e2fd4d83fada812dd3c066fa1ee31683dd00
zaynzzz <89463457+zaynzzz@users.noreply.github.com>
2022-04-12 16:30:21 +0700

    update 12-04-2022
    update

--- app/Views/admin/layout/menu.php
+++ app/Views/admin/layout/menu.php
@@ -16,8 +16,8 @@ $users = db_connect()->query('SELECT * FROM users')->getRow();
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
 
-  <a href="<?= base_url('/Dashboard'); ?>" class="brand-link">
-    <img src="<?= base_url('assets/upload/image/' . $site['icon']) ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+  <a href="<?= base_url(); ?>" class="brand-link">
+    <img src="<?= base_url('assets/upload/image/' . $site['icon']) ?>" alt="View Your Website" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-primary"><?= $site['namaweb'] ?></span>
   </a>
   <!-- Sidebar -->
